fc58cebc865655e8ef5930404c351586c2912af5
Sammy Khalife <Sammy.Khalife@fr.eurw.ey.net>
2016-04-25 11:54:03 +0200

    New organization. Firebase connexion working

--- app.py
+++
@@ -1,5 +0,0 @@
-# example from http://ozgur.github.io/python-firebase/
-from firebase import firebase
-firebase = firebase.FirebaseApplication('https://boiling-fire-1432.firebaseio.com/', None)
-result = firebase.get('/users', None)
-print(result)
---
+++ firebaseConnexion.py
@@ -0,0 +1,17 @@
+# Based on example from http://ozgur.github.io/python-firebase/
+from firebase import firebase
+
+# firebase = firebase.FirebaseApplication('https://boiling-fire-1432.firebaseio.com/', None)
+# data = {'name': 'Actuarial services', 'location' : 'Paris', 'created at': 2016} 
+# firebase.post('/locationData', data)
+# result = firebase.get('/X', None)
+# print(result)
+
+def pushFireBase(data, url):
+	try:
+		firebase = firebase.FirebaseApplication(url, None)
+		firebase.delete('tweetData')
+		firebase.post('tweetData', data)
+		return True
+	except:
+		return False
